c1e3e003472419018a2df6d13903fa29938a3391
Chris Koehler <acomputerdog@gmail.com>
2020-08-15 13:11:24 -0400

    Script fixes for build-and-test action

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,11 +1,25 @@
 const fs = require('fs');
 
-const rmOptions = {
-    recursive: true,
-    maxRetries: 5,
-    retryDelay: 250
-};
+function removePath(path) {
+    // skip if it doesn't exist
+    if (fs.existsSync(path)) {
+        // get details
+        const pathStat = fs.statSync(path);
 
-fs.rmdirSync('dist', rmOptions);
-fs.rmdirSync('docs', rmOptions);
-fs.rmdirSync('.nyc_output', rmOptions);
+        if (pathStat.isFile()) {
+            // if file, then delete
+            fs.unlinkSync(path);
+        } else if (pathStat.isDirectory()) {
+            // if directory, then recursively delete
+            fs.rmdirSync(path, {
+                recursive: true,
+                maxRetries: 5,
+                retryDelay: 250
+            });
+        }
+    }
+}
+
+removePath('dist');
+removePath('docs');
+removePath('.nyc_output');
